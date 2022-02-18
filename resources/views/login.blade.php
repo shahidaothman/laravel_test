bcda3d1eb95b561b56308b7e2fab21a417117991
Nur Shahida Othman <nurshahidaothman@Chul.local>
2022-02-18 15:56:48 +0800

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,112 +3,395 @@
 
 <head>
 
-    
-    <meta charset="utf-8" />
-    <title>Login page | Morvin - Admin & Dashboard Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesdesign" name="author" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
 
-    <!-- Bootstrap Css -->
-    <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+  <meta charset="utf-8" />
+  <title>Horizontal | Morvin - Admin & Dashboard Template</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+  <meta content="Themesdesign" name="author" />
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico">
 
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <!-- plugin css -->
+  <link href="assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+
+  <!-- Bootstrap Css -->
+  <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+  <!-- Icons Css -->
+  <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+  <!-- App Css-->
+  <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+
+
 </head>
 
 
-<body class="authentication-bg bg-primary">
-    <div class="home-center">
-        <div class="home-desc-center">
+<body data-topbar="dark" data-layout="horizontal">
 
-            <div class="container">
-
+  <!-- Begin page -->
+  <div id="layout-wrapper">
 
 
+    <header id="page-topbar">
+      <div class="navbar-header">
+        <div class="d-flex">
+          <!-- LOGO -->
+          <div class="navbar-brand-box">
+            <a href="index.html" class="logo logo-dark">
+              <span class="logo-sm">
+                <img src="assets/images/logo-sm.png" alt="" height="22">
+              </span>
+              <span class="logo-lg">
+                <img src="assets/images/logo-dark.png" alt="" height="20">
+              </span>
+            </a>
 
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="px-2 py-3">
+            <a href="index.html" class="logo logo-light">
+              <span class="logo-sm">
+                <img src="assets/images/logo-sm.png" alt="" height="22">
+              </span>
+              <span class="logo-lg">
+                <img src="assets/images/logo-light.png" alt="" height="20">
+              </span>
+            </a>
+          </div>
+
+     
 
 
-                                    <div class="text-center">
-                                        <a href="index.html">
-                                            <img src="assets/images/logo-dark.png" height="22" alt="logo">
-                                        </a>
 
-                                        <h5 class="text-primary mb-2 mt-4">Welcome Back !</h5>
-                                        <p class="text-muted">Your Needs & Solutions
-</p>
-                                    </div>
+        </div>
+
+        <!-- Search input -->
+        <div class="search-wrap" id="search-wrap">
+          <div class="search-bar">
+            <input class="search-input form-control" placeholder="Search" />
+            <a href="#" class="close-search toggle-search" data-target="#search-wrap">
+              <i class="mdi mdi-close-circle"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="d-flex">
+
+          <div class="dropdown d-none d-lg-inline-block">
+            <button type="button" class="btn header-item toggle-search noti-icon waves-effect" data-target="#search-wrap">
+              <i class="mdi mdi-magnify"></i>
+            </button>
+          </div>
 
 
-                                    <form class="form-horizontal mt-4 pt-2" action="index.html">
+          <div class="dropdown d-none d-md-block ms-2">
+            <button type="button" class="btn header-item waves-effect" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img class="me-2" src="assets/images/flags/us.jpg" alt="Header Language" height="16"> English <span class="mdi mdi-chevron-down"></span>
+            </button>
+            <div class="dropdown-menu dropdown-menu-end">
 
-                                        <div class="mb-3">
-                                            <label for="username">Username</label>
-                                            <input type="text" class="form-control" id="username"
-                                                placeholder="Enter username">
-                                        </div>
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <img src="assets/images/flags/germany.jpg" alt="user-image" class="me-1" height="12"> <span class="align-middle"> German </span>
+              </a>
 
-                                        <div class="mb-3">
-                                            <label for="userpassword">Password</label>
-                                            <input type="password" class="form-control" id="userpassword"
-                                                placeholder="Enter password">
-                                        </div>
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <img src="assets/images/flags/italy.jpg" alt="user-image" class="me-1" height="12"> <span class="align-middle"> Italian </span>
+              </a>
 
-                                        <div class="mb-3">
-                                                <div class="form-check">
-                                                    <input type="checkbox" class="form-check-input"
-                                                        id="customControlInline">
-                                                    <label class="form-label"
-                                                        for="customControlInline">Remember me</label>
-                                                </div>
-                                        </div>
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <img src="assets/images/flags/french.jpg" alt="user-image" class="me-1" height="12"> <span class="align-middle"> French </span>
+              </a>
 
-                                        <div>
-                                            <a class="btn btn-primary w-100 waves-effect waves-light" href="{{ url('client') }}"
-                                                type="submit">Log In</a>
-                                        </div>
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <img src="assets/images/flags/spain.jpg" alt="user-image" class="me-1" height="12"> <span class="align-middle"> Spanish </span>
+              </a>
 
-                                        <div class="mt-4 text-center">
-                                            <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a>
-                                        </div>
-    
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <img src="assets/images/flags/russia.jpg" alt="user-image" class="me-1" height="12"> <span class="align-middle"> Russian </span>
+              </a>
+            </div>
+          </div>
 
-                                    </form>
+          <div class="dropdown d-none d-lg-inline-block ms-1">
+            <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
+              <i class="mdi mdi-fullscreen"></i>
+            </button>
+          </div>
 
-                                  
-                                </div>
+
+
+         
+
+         
+
+        
+
+        </div>
+      </div>
+    </header>
+
+    <div class="topnav">
+      <div class="container-fluid">
+        <nav class="navbar navbar-light navbar-expand-lg topnav-menu">
+
+          <div class="collapse navbar-collapse" id="topnav-menu-content">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                  <i class="dripicons-home me-2"></i> Home
+                </a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-apps" role="button">
+                  <i class="dripicons-archive me-2"></i> Loan <div class="arrow-down"></div>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="topnav-apps">
+
+                  <a href="calendar.html" class="dropdown-item">Personal</a>
+
+                  <a href="chat.html" class="dropdown-item">Business</a>
+                  <a href="chat.html" class="dropdown-item">Home</a>
+              
+                  <a href="chat.html" class="dropdown-item">Car</a>
+                  <a href="chat.html" class="dropdown-item">Educational Plan</a>
+                  <a href="chat.html" class="dropdown-item">Other</a>
+                </div>
+              </li>
+
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-apps" role="button">
+                  <i class="dripicons-archive me-2"></i> Apps <div class="arrow-down"></div>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="topnav-apps">
+                <a href="chat.html" class="dropdown-item">Download</a>
+
+                  <a href="calendar.html" class="dropdown-item">Calendar</a>
+
+                  <a href="chat.html" class="dropdown-item">Chat</a>
+
+
+                </div>
+              </li>
+
+           
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-apps" role="button">
+                  <i class="dripicons-archive me-2"></i> Branch <div class="arrow-down"></div>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="topnav-apps">
+
+                  <a href="calendar.html" class="dropdown-item">Location</a>
+
+                  <a href="chat.html" class="dropdown-item">Appointment Branch</a>
+
+              
+
+
+                </div>
+              </li>
+
+            
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-apps" role="button">
+                  <i class="dripicons-archive me-2"></i> Career <div class="arrow-down"></div>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="topnav-apps">
+
+                  <a href="calendar.html" class="dropdown-item">Job Opening</a>
+
+
+                </div>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-apps" role="button">
+                  <i class="dripicons-archive me-2"></i> Contact Us 
+                </a>
+             
+              </li>
+
+           
+
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+
+      <div class="page-content">
+
+        <!-- start page title -->
+        <div class="page-title-box">
+          <div class="">
+            <div class="row align-items-center">
+      
+              
+            </div>
+          </div>
+        </div>
+        <!-- end page title -->
+
+
+        <div class="container-fluid">
+
+          <div class="page-content-wrapper">
+
+            <div class="row">
+              <div class="col-xl-12">
+                <div class="card">
+                  <div class="card-body">
+
+
+
+
+
+
+
+                    <div class="clearfix"></div>
+
+
+                    <div class="row align-items-center">
+                      <div class="col-xl-8">
+
+                        <div>
+                          <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-interval="3000">
+                            
+                            <div class="carousel-inner" role="listbox">
+                              <div class="carousel-item">
+                                <img class="d-block img-fluid" src="assets/images/small/img-1.jpg" alt="First slide">
+                              </div>
+                              <div class="carousel-item active">
+                                <img class="d-block img-fluid" src="assets/images/small/img-2.jpg" alt="Second slide">
+                              </div>
+                              <div class="carousel-item">
+                                <img class="d-block img-fluid" src="assets/images/small/img-3.jpg" alt="Third slide">
+                              </div>
                             </div>
+                          </div>
                         </div>
 
-                    
+                      </div>
+
+
+                      <div class="col-xl-4">
+                        <div class="px-2 py-3">
+
+
+                          <div class="text-center">
+                            <a href="index.html">
+                              <img src="assets/images/logo-dark.png" height="22" alt="logo">
+                            </a>
+
+                            <h5 class="text-primary mb-2 mt-4">Welcome Back !</h5>
+                            <p class="text-muted">Your Needs & Solutions
+                            </p>
+                          </div>
+
+
+                          <form class="form-horizontal mt-4 pt-2" action="index.html">
+
+                            <div class="mb-3">
+                              <label for="username">Username</label>
+                              <input type="text" class="form-control" id="username" placeholder="Enter username">
+                            </div>
+
+                            <div class="mb-3">
+                              <label for="userpassword">Password</label>
+                              <input type="password" class="form-control" id="userpassword" placeholder="Enter password">
+                            </div>
+
+                            <div class="mb-3">
+                              <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="customControlInline">
+                                <label class="form-label" for="customControlInline">Remember me</label>
+                              </div>
+                            </div>
+
+                            <div>
+                              <a class="btn btn-primary w-100 waves-effect waves-light" href="{{ url('client') }}" type="submit">Log In</a>
+                            </div>
+
+                            <div class="mt-4 text-center">
+                              <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a>
+                            </div>
+
+
+                          </form>
+
+
+                        </div>
+                      </div>
+
+
                     </div>
+
+
+                  </div>
                 </div>
+              </div>
+
+
+
 
             </div>
 
 
-        </div>
-        <!-- End Log In page -->
+
+
+          </div>
+
+
+        </div> <!-- container-fluid -->
+
+
+      </div>
+      <!-- End Page-content -->
+
+
+
+
     </div>
 
-    <!-- JAVASCRIPT -->
-    <script src="assets/libs/jquery/jquery.min.js"></script>
-    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/libs/metismenu/metisMenu.min.js"></script>
-    <script src="assets/libs/simplebar/simplebar.min.js"></script>
-    <script src="assets/libs/node-waves/waves.min.js"></script>
 
-    <script src="assets/js/app.js"></script>
+
+
+  </div>
+  <!-- END layout-wrapper -->
+
+
+  <!-- /Right-bar -->
+
+  <!-- Right bar overlay-->
+  <div class="rightbar-overlay"></div>
+
+  <!-- JAVASCRIPT -->
+  <script src="assets/libs/jquery/jquery.min.js"></script>
+  <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+  <script src="assets/libs/simplebar/simplebar.min.js"></script>
+  <script src="assets/libs/node-waves/waves.min.js"></script>
+
+  <!-- apexcharts -->
+  <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+
+  <!-- Plugins js-->
+  <script src="assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
+
+  <script src="assets/js/pages/dashboard.init.js"></script>
+
+
+  <script src="assets/js/app.js"></script>
 
 </body>
 
